9430f157e03251680db8b44589398d9c1e5e7a2f
Anderson <alsanderson2@gmail.com>
2022-06-11 10:56:07 -0300

    atualizando tabela
    Atualizando a tabela  é alterando o botão do formulário para do tipo button para input.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,40 @@
-let dados = [
-          {
-           compra: document.getElementById('#compra')
-          },
-          {
-           venda: document.getElementById('#venda')    
-          },
-          {
-           nome: document.getElementById('#input_nome')    
-          },     
-          {
-           valor: document.getElementById('#input_valor')  
-          }        
-     ];  
+     function extratos() {
+          let dados = [
+               {
+               compra: document.getElementById('compra').value,
+               vendas: document.getElementById('venda').value,    
+               nome: document.getElementById('input_nome').value,    
+               valor: document.getElementById('input_valor').value,  
+               }        
+          ];  
+     }
 
      function transacao() { 
-     localStorage.setItem('extrato', JSON.stringify(dados));
+     localStorage.setItem('extrato', JSON.stringify(extrato()));
      let recebestring = localStorage.getItem('extrato')
      recebeobjto = JSON.parse(recebestring)
      recebeobjto  = document.querySelector('table') 
      recebeobjto.innerHTML +=`
-          <tr ><td class="mais">${(dados[0].compra)}</td>
-          <td class="descricao">${dados[2].nome}</td>
-          <td class="soma">${dados[3].valor}<td><tr>`
+          <tr ><td class="mais">${(dados[0])}</td>
+          <td class="descricao">${dados[3]}</td>
+          <td class="soma">${dados[4]}<td><tr>`
      } 
-
-
-     function limpar() {
-          window.confirm('Deseja excluir os dados?')
-          if (true){
-               localStorage.clear('extrato')
+     
+     function limpatabela() {
+          while ((localStorage.clear == true)) {
+               dados = recebeobjto.splice(recebeobjto)
           }
      }
-         /* }else (true); {
+     function limpar() {
+          let sim = window.confirm('Deseja excluir os dados?')
+          if (sim){
+               localStorage.clear('extrato')
+               limpatabela()
+          }
+     }
+
+     
+         /* else (true); 
               `<tr >
                <td class="mais">-</td>
                <td class="descricao">${dados[person].nome}</td>
