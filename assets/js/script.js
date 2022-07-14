b3583cafd6c6c7df3f1dd4e891cb91ca43cce024
Anderson <alsanderson2@gmail.com>
2022-07-14 08:09:02 -0300

    Mascara
    Alteração de componentes da mascara de valor monetário.
    Atualizando pequenos bugs no botão de limpar e otimizando o código para que rode de maneira mais fluida.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,26 +8,23 @@ if(localStorage.getItem('dados')) {
 }
 
 //abrir o menu
-var abrirMenu = document.querySelector('.menu')
-abrirMenu.addEventListener('click',(e)=>{
-    e.preventDefault()
-    let menuMobile = document.querySelector('nav')
-    menuMobile.classList.add('mostranav')
-})
+var abrirFecar = document.querySelector('.menu')
+abrirFecar.addEventListener('click',abriFecha )
+function abriFecha() {
+    document.querySelectorAll('nav')[0].classList.toggle('abrir')
+}
 //fechar o menu
-var fechaMenu = document.querySelector('.p')
-fechaMenu.addEventListener('click',(e)=>{
-    e.preventDefault()
-    let mobileMenu = document.querySelector('nav')
-    mobileMenu.classList.add('escondeMenu')
-})
+var abrirFecar = document.querySelector('.fechaMenu')
+abrirFecar.addEventListener('click', esconde )
+function esconde() {
+    document.querySelectorAll('nav')[0].classList.toggle('abrir')
+}   
 
 //Função para mostrar os dados do localstorage na pagina.
 function transacao() {
     let dados = JSON.parse(localStorage.getItem('dados'));
     let recebeobjto = document.querySelector('thead')
-
-
+    
     /*Verificar se o localstorage está nulo (sem dados).
     Caso não esteja , recebe uma função map().
     Essa função lista todas as keys do localstorage e mostra no HTML.*/
@@ -43,85 +40,80 @@ function transacao() {
         })
         .join('') //Junta todos os elementos do dados.map e retorna uma string.
         sinal()
-        somaValores()
-    }
+        somaValores()      
+    } 
 }
 
 //Função para somar os valores do input valor.
 function somaValores() {
     var total = 0;
-    let valorinput;
-
+    var valordoinput
     /*Verificar se o select está selecionado igual a "compra".
-   Caso esteja selecionado o sinal e correspondido (alterado).*/
-   for(produto in dados) {
+    Caso esteja selecionado o sinal e correspondido (alterado).*/
+    for(produto in dados) {  
         if(dados[produto].selecao == 'compra') {
-            valorInput = dados[produto];
+            valordoinput = dados[produto];
             total -= Number(dados[produto].valor)
         } else {
             total += Number(dados[produto].valor)
-        }
-   }
-
-   /*Verificar se o temanho dos dados e maior que zero.
-   Caso seja maior o total é mostrado com o status [LUCRO] ou [PREJUIZO].*/
-   if(dados.length > 0) {
-        let resultobj = document.querySelector('.thead-b')
-        resultobj.innerHTML = `
-            <tr class="lucro_preju">
-                <td>Total</td>
-                <td>${formatterCurrency(Number(total))}</td>
-            </tr>`   
-        resultobj.innerHTML += `
-            <tr class="tr_d">
-                <td>${Math.sign(total) > 0 ? '[LUCRO]' : '[PREJUÍZO]'}</td>
-            </tr>`       
-   }
-}
-
-/*função para deletar os dados do local storage .
-Se não tem nenhuma transação cadastrada aparece um alert.*/
-function limpar () {
-    if(dados.length <= 0) {
-        window.alert('Nenhum registro de transações!')
-    } else {
-        var caixaTexto = window.confirm('Deseja excluir as imformações?')
-        if (caixaTexto == true) {
-            localStorage.clear()
-            window.alert('Informações excluidas!')
-        } else {
-            window.alert('Exclusões canceladas!')
-        }
+        }          
     }
-    transacao()
-    location.reload() //atualizar a pagina
+
+    /*Verificar se o temanho dos dados e maior que zero.
+    Caso seja maior o total é mostrado com o status [LUCRO] ou [PREJUIZO].*/  
+    if(dados.length > 0) {
+        let resultobj = document.querySelector('.total')
+        resultobj.innerHTML = `
+        <tr class="lucro_preju">
+            <td>Total</td>
+            <td>${formatterCurrency(Number(total < 0 ? total*-1 : total))}</td>
+        </tr>`    
+        let lucPrej = document.querySelector('.tr-saldolucro')       
+        lucPrej.innerHTML = `
+        <tr class="tr_d">
+            <td>${Math.sign (total) > 0 ? '[LUCRO]' : '[PREJUÍZO]'}</td>
+        </tr>`
+    }           
 }
-
-/*O link (limpar dados) fica na espera (addEventListener) para ser executado ao ser clicado  */
-let linkLimpar = document.querySelector('.li_limpar')
-linkLimpar.addEventListener('click', limpar)
-
-//Funçao para a formataão monetári no Brasil (BRL = R$)
+/*
+// função para apagar os dados do extrato e desenha novamente o extrato vazio
+function atualizar() {
+    let tabela = document.querySelector('.tabela')
+    tabela.innerHTML = `
+    <table>
+        <tr class="tr_a">
+            <th>Mercadoria</t>
+            <th>Valor</th>
+        </tr>
+        <thead>    
+            <tr class="tr_b">
+                <td class="td_mercadoria">
+                    Nenhuma mercadoria cadastrada.
+                </td>
+            </tr>
+        </thead>
+    </table>`                 
+}
+*/
+// Funçao para o formato monetário no Brasil (BRL = R$)
 function formatterCurrency(value) {
-    const valueFormat = value.toLocaleString('pt-BR',{
-      style:'currency',
-      currency:'BRL',
-   })
-   return valueFormat;
+    const valueFormat = value.toLocaleString('pt-br',{style: 'currency',  currency: 'BRL'});
+    return valueFormat;
 }
-
+    
 /*Mascara para formatar o input valor automaticamente no padrão monetário brasileiro (BRL = R$) */
-function moeda() {
-    let elemento = document.getElementById('input_valor')
-    let valor = elemento.value
+function moeda(e) {
+    var elemento = document.getElementById('input_valor')
+    var valor = elemento.value
 
-    valor = parseInt(valor.replace(/[\D]+/g, ''))
-    valor = valor + ''
-    valor = valor.replace(/([0-9]{2})$/g, ',$1')
-
-    elemento.value = valor
-    if(valor == 'NaN') elemento.value = ''
+    valor = valor.replace(/[\.,]/g, '') 
+    valor + e.key
+    valor = parseFloat(valor.replace(/([0-9]{2})$/,".$1"))
+    elemento.value = valor.toLocaleString('pt-br',{minimumFractionDigits: 2})
+    if(typeof NaN === typeof 0)
+    e.preventDefault()
 }
+
 //Função para mudar o sinal de acordo com a seleção compra ou venda.
 function sinal() {
     let dados = JSON.parse(localStorage.getItem('dados'))
@@ -135,6 +127,28 @@ function sinal() {
         }
     }
 }
+
+/*O link (limpar dados) fica na espera (addEventListener) para ser executado ao ser clicado  */
+let linkLimpar = document.querySelector('.li_limpar')
+linkLimpar.addEventListener('click', limpar)
+
+/*função para deletar os dados do local storage .
+Se não tem nenhuma transação cadastrada aparece um alert.*/
+function limpar () {
+    if(dados.length <= 0) {
+        window.alert('Nenhum registro de transações!')
+    }else {
+        var caixaTexto = confirm('Deseja excluir as imformações?')
+        if (caixaTexto == true) {  
+           localStorage.clear()  
+           atualizaIndex()
+        }else {
+            window.alert('Exclusões canceladas!')
+        }  
+    }    
+    transacao()
+    somaValores()   
+} 
 
 /*Função que para de atualizar a página ao clicar no botão subit (adicionar transação)*/
 function validacao(event) {
@@ -150,7 +164,7 @@ function validacao(event) {
         nome:nome,
 
         //replaceALL faz a troca de ponto para espaço e de virgula para ponto.
-        valor: valor.replaceAll('.', '').replaceAll(',','.'),
+        valor: valor.replaceAll('.','').replaceAll(',','.')
     })
 
     /*Transforma os dados do loaclstorage em string.
@@ -158,6 +172,11 @@ function validacao(event) {
     let dadosString = JSON.stringify(dados)
     localStorage.setItem('dados', dadosString)
 
-    //mostrar novamente a página com os dados atualizados
-    transacao();
+    //mostrar novamente a página com os dados atualizados    
+    transacao()
+}
+
+//função para carregar a página inicial index
+function atualizaIndex() {
+    location.href="./index.html"
 }
